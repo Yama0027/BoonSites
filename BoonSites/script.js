6771f2c118f31c8d9efa236bb5493bb62a32c9d5
Yama0027 <yharuto0019@gmail.com>
2025-09-15 17:50:39 +0900

    Delete BoonSites directory

--- BoonSites/script.js
+++
@@ -1,6 +0,0 @@
-const explodeButton = document.getElementById('explodeButton');
-const explosionSound = document.getElementById('explosionSound');
-
-explodeButton.addEventListener('click', () => {
-    explosionSound.play();
-});
